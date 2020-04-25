8612b20dfc9314ffae6f05114f52f56255fb54af
Kakaranish <stasiekgruz@gmail.com>
2020-04-25 19:15:36 +0200

    Created user model

---
+++ src/models/user-model.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import emailValidator from 'email-validator';
+const Schema = mongoose.Schema;
+
+const availableRoles = [
+    "USER",
+    "ADMIN"
+];
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.path('email').validate(email =>
+    emailValidator.validate(email), "invalid 'email'"
+);
+
+userSchema.path('role').validate(role =>
+    availableRoles.includes(role), "invalid 'role'"
+);
+
+userSchema.post('save', (error, doc, next) => {
+    if (error.name === 'MongoError' && error.code === 11000) {
+        next(new Error("'email' must be unique"));
+    } else {
+        next(error);
+    }
+});
+
+const UserModel = mongoose.model('user', userSchema);
+
+export default UserModel;
