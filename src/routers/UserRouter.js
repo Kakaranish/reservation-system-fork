ed664ad01b43573d0f93fa2c8d263b6884bbabbd
Kakaranish <stasiekgruz@gmail.com>
2020-05-03 19:37:18 +0200

    + changed logic in UserRouter to use mongoose instead of native mongodb driver + ultimately removed DbClient and DbQueries

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,26 +1,19 @@
 import express from "express";
-import dbClient from '../DbClient';
-import passport from "passport";
-import { ObjectID } from "mongodb";
-require('../auth');
-const dbActions = require('../DbQueries');
-import mongoose from 'mongoose';
-import Reservation from '../models/reservation-model';
+import { changeReservationStatus } from "../DbQueries2";
+import User from '../models/user-model';
 import FindReservationQueryBuilder from '../queries/FindReservationQueryBuilder';
 import { parseObjectId } from '../common';
-import { userValidator, adminValidator, authValidator } from '../auth-validators';
-import { query, validationResult } from 'express-validator';
+import { userValidator } from '../auth-validators';
+import { body, query, validationResult } from 'express-validator';
 
 const router = express();
-const resSystemDbClient = dbClient();
-
 
 router.get('/reservations', [
     userValidator,
     query('status').notEmpty().withMessage('cannot be empty').bail()
         .custom(status => {
             const availableStatuses = ['PENDING', 'ACCEPTED', 'REJECTED', 'CANCELLED'];
-            if (!availableStatuses.includes(status))
+            if (!availableStatuses.includes(status.toUpperCase()))
                 throw Error('illegal status');
             return true;
         })
@@ -43,61 +36,35 @@ router.get('/reservations', [
     }
 });
 
-
-router.post('/cancel-reservation/:reservationId', async (req, res) => {
-    passport.authenticate('jwt', { session: false }, async (error, user) => {
-        if (!user.role) return res.status(401).json({
-            message: "Unauthorized access"
-        });
-        if (user.role !== "USER") return res.status(401).json({
-            message: "OK admin. Only normal user can do this"
-        });
-
-        if (!ObjectID.isValid(req.params.reservationId)) {
-            return res.status(400).json({
-                message: `Error: ${req.params.reservationId} is not valid reservationId`
-            });
-        }
-        const reservationId = new ObjectID(req.params.reservationId);
-        try {
-            const pendingReservations = await resSystemDbClient.withDb(async db => {
-                if (!await dbActions.reservationWithIdExists(db, reservationId)) return null;
-                await dbActions.changeReservationStatus(db, reservationId, "CANCELLED");
-                return true;
-            });
-            if (!pendingReservations) return res.status(400).json({
-                message: `Error: Reservation with id ${reservationId} does not exist`
-            });
-            return res.status(200).json({
-                message: "OK"
-            });
-        } catch (error) {
-            console.log(`Error: ${error}`);
-            res.status(500).json({
-                message: "Error: Internal server error"
-            });
-        }
-    })(req, res);
-});
-
-router.post('/check-if-email-available', async (req, res) => {
-    const userEmail = req.body.email;
-    if (!userEmail) return res.status(400).json({
-        message: "'email' was not provided"
-    });
-
+router.post('/cancel-reservation/:reservationId', [
+    userValidator,
+    body('reservationId').customSanitizer(roomId => parseObjectId(roomId))
+        .notEmpty().withMessage('invalid mongo ObjectId'),
+], async (req, res) => {
+    if (validationResult(req).errors.length > 0)
+        return res.status(400).json(validationResult(req));
     try {
-        const userExists = await resSystemDbClient.withDb(async db => {
-            return await dbActions.userWithEmailExists(db, userEmail);
-        });
-        return res.status(200).json({
-            "available": !userExists
-        });
+        const result = await changeReservationStatus(req.body.reservationId, 'CANCELLED');
+        return res.status(200).json({_id: result?._id ?? null});
     } catch (error) {
         console.log(`Error: ${error}`);
-        res.status(500).json({
-            message: "Error: Internal server error"
-        });
+        res.status(500).json({ errors: ['Internal error'] });
+    }
+});
+
+router.post('/check-if-email-available', [
+    body('email').isString().withMessage('must be string').bail()
+        .notEmpty().withMessage('cannot be empty').bail()
+        .isEmail().withMessage('invalid email format')
+], async (req, res) => {
+    if (validationResult(req).errors.length > 0)
+        return res.status(400).json(validationResult(req));
+    try {
+        const available = !await User.exists({ email: req.body.email })
+        return res.status(200).json(available);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        res.status(500).json({ errors: ['Internal error'] });
     }
 });
 
