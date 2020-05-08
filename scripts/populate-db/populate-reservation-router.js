1ea64c2df3106ef59fff65754aad59fe7bb424ca
Kakaranish <stasiekgruz@gmail.com>
2020-05-08 15:41:14 +0200

    + test db population split into separate files - step 1

---
+++ scripts/populate-db/populate-reservation-router.js
@@ -0,0 +1,4 @@
+
+export const populateReservations = (async () => {
+
+});
---
+++ scripts/populate-db/populate.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import moment, { ISO_8601 } from 'moment';
+import User from '../../src/models/user-model';
+import Room from '../../src/models/room-model';
+import Reservation from '../../src/models/reservation-model';
+import { parseObjectId } from '../../src/common';
+import { connectTestDb } from '../../src/mongo-utils';
+import { createRefreshToken } from '../../src/auth/auth-utils';
+import * as Common from './populate-common';
+import {populateReservations} from "./populate-reservation-router"
+import { populateTemp } from './temp';
+
+require('dotenv').config();
+
+(async () => {
+    await connectTestDb();
+    await mongoose.connection.dropDatabase();
+
+    // ---  POPULATE COMMON  ---------------------------------------------------
+
+    await Common.commonUser.save();
+    await createRefreshToken(Common.commonUser);
+    await Common.commonAdmin.save();
+    await createRefreshToken(Common.commonAdmin);
+
+    // ---  POPULATE  ----------------------------------------------------------
+
+    await populateReservations();
+    await populateTemp();
+
+    // -------------------------------------------------------------------------
+
+    await mongoose.connection.close();
+    const dbName = process.env.DB_NAME_TEST;
+    console.log(`OK - '${dbName}' has been populated.`);
+})();
